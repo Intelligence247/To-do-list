8d13a60dc06bdbb2b744f7889895ace5b6ec675a
Intelligence247 <115150772+Intelligence247@users.noreply.github.com>
2023-03-08 01:07:38 +0100

    form completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,54 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+    const initialtodo=['wait']
+  const [todo, settodo] = useState(initialtodo)
+  const [inputext, setInputext] = useState([])
+  const [err, setErr] = useState('')
+  const [cancel, setCancel] = useState(false)
+  const [display, setDisplay] = useState(true)
+  const handleclick =(e)=>{
+    e.preventDefault();
+    if(inputext.length<1){
+      setErr('Input box must not be empty')
+    }else{
+      setErr('')
+  settodo(todo.concat(inputext))
+  setInputext('');
+  }
+}
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="body">
+     <main>
+    <h1 className='text-[2rem] w-max m-auto'>Todo List</h1>
+       <form action="">
+    <div className="inbut">
+    <input 
+    type="text" 
+    value={inputext}
+    onChange={(e)=>setInputext(e.target.value)}/>
+    <button type='submit' className='btnadd' onClick={handleclick}>Add</button>
+    </div>
+<div className={`err`}>{err}</div>
+    <div className="lists"
+    >
+      {todo.map((t,i)=>(
+    <div
+     className={`todo ${cancel? 'cancel':''}`}
+     onClick={()=>setCancel(!cancel)}
+    key={i}
+    >
+     <input type="checkbox" id="" checked={cancel} />
+      <p className={`${cancel&&'lineover'}`}>{t}</p>
+      <img src="/media/del.png" alt=""
+      onClick={()=>setDisplay(!display)} />
+    </div>
+
+      ))}
+
+    </div>
+       </form>
+     </main>
     </div>
   )
 }
